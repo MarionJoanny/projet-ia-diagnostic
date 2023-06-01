3ef670da2b159f9d86d890cfef4ddafe1df79341
Emilien Andre <emilien.andre@idakto.com>
2023-06-01 13:00:38 +0200

    fin de seance 01/06

--- projet_rp.py
+++ projet_rp.py
@@ -45,12 +45,54 @@ df_OT.drop(columns=['KILOMETRAGE'], inplace=True)
 #print(df_OT.head(100))
 
 
-print(df_equipe['MOTEUR'].unique())
-print(df_equipe['EQU_ID'].unique())
+#print(f'moteur {df_equipe["MOTEUR"].unique()}')
+#print(df_equipe['EQU_ID'].unique())
 
-print(df_OT['SIG_ORGANE'].unique())
-print(df_OT['ODR_LIBELLE'].value_counts())
-print(df_OT['TYPE_TRAVAIL'].unique())
-print(df_OT['EQU_ID'].unique())
-print(df_OT['SYSTEM_N1'].value_counts())
+#print(df_OT['SIG_ORGANE'].unique())
+#print(df_OT['ODR_LIBELLE'].value_counts())
+#print(df_OT['TYPE_TRAVAIL'].unique())
+#print(df_OT['EQU_ID'].unique())
+#print(df_OT['SYSTEM_N1'].value_counts())
 
+
+## RAPHAEL SPACE HERE
+##jointure des dataframes
+df_light = df_OT[['SIG_ORGANE', 'SYSTEM_N1', 'EQU_ID']]
+
+df_light = df_light.merge(df_equipe, on='EQU_ID', how='left')
+print('####################################################')
+df_light.drop(columns=['CONSTRUCTEUR'], inplace=True)
+print(df_light.head(10))
+
+
+import sklearn.model_selection.train_test_split as tts
+x_train, x_test, y_train, y_test = tts(df_light['MODELE','MOTEUR','SIG_ORGANE'],df_light['SYSTEM_N1'], test_size=0.2, random_state=42)
+
+print(x_train.shape)
+print(x_test.shape)
+
+## RAPHAEL SPACE HERE
+
+print('####################################################')
+
+import pyAgrum as gum
+
+bn = gum.BayesNet()
+
+mot = bn.add(gum.LabelizedVariable('MOTEUR', 'le moteur', 56))
+mdl = bn.add(gum.LabelizedVariable('MODELE', 'le modele', 68))
+org = bn.add(gum.LabelizedVariable('SIG_ORGANE', 'l\'organe', len(df_OT['SIG_ORGANE'].cat.categories)))
+sys = bn.add(gum.LabelizedVariable('SYSTEM_N1', 'localisation', len(df_OT['SYSTEM_N1'].cat.categories)))
+
+bn=gum.fastBN("sys<-mot<-mdl->org->sys")
+
+print(bn)
+
+def compute_probas(variable):
+    probas = {}
+    probas.setdefault(0)
+    for _,val in variable:
+        probas[val] += 1
+
+comptage = compute_probas(df_OT['SIG_ORGANE'])
+print(comptage)
