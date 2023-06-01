0f10f88b02ce8a8ffe28b5be4deb0c042d743a41
rhn <heurlin.e2105233@etud.univ-ubs.fr>
2023-06-01 11:23:24 +0200

    pause café 11:30

--- projet_rp.py
+++ projet_rp.py
@@ -7,6 +7,7 @@ file_OT = open("../OT_ODR.csv", "r")
 df_equipe = pd.read_csv(file_equip, sep=";")
 df_OT = pd.read_csv(file_OT, sep=";")
 
+## description des dataframes
 
 print('-------------equipe-------------------------')
 
@@ -25,8 +26,19 @@ print('----------------OT----------------------')
 print(df_OT.describe())
 print(df_OT.head())
 print(f'isna = {df_OT.isna().sum()}')
+
+
 for i in df_OT.columns:
     print(f'################    {i}     #########################')
     print(df_OT[i].unique())
 print('--------------------------------------')
 
+
+
+## cleaning des dataframes
+
+print(df_OT.head(10))
+print(df_OT[df_OT['KILOMETRAGE'].isna() == True])
+
+df_OT.drop(columns=['KILOMETRAGE'], inplace=True)
+print(df_OT.head(100))
