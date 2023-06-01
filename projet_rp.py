f8d6fe6c401550ad4f47b79a117a32a27047b108
rhn <heurlin.e2105233@etud.univ-ubs.fr>
2023-06-01 12:05:49 +0200

    selection variable

--- projet_rp.py
+++ projet_rp.py
@@ -15,9 +15,10 @@ print('-------------equipe-------------------------')
 print(df_equipe.describe())
 print(df_equipe.head())
 print(f'isna = {df_equipe.isna().sum()}')
-for i in df_equipe.columns:
-    print(f'################    {i}     #########################')
-    print(df_equipe[i].unique())
+
+#for i in df_equipe.columns:
+    #print(f'################    {i}     #########################')
+    #sprint(df_equipe[i].unique())
 
 print('--------------------------------------')
 
@@ -28,17 +29,28 @@ print(df_OT.head())
 print(f'isna = {df_OT.isna().sum()}')
 
 
-for i in df_OT.columns:
-    print(f'################    {i}     #########################')
-    print(df_OT[i].unique())
+#for i in df_OT.columns:
+    #print(f'################    {i}     #########################')
+    #print(df_OT[i].unique())
 print('--------------------------------------')
 
 
 
 ## cleaning des dataframes
 
-print(df_OT.head(10))
-print(df_OT[df_OT['KILOMETRAGE'].isna() == True])
+#print(df_OT.head(10))
+#print(df_OT[df_OT['KILOMETRAGE'].isna() == True])
 
 df_OT.drop(columns=['KILOMETRAGE'], inplace=True)
-print(df_OT.head(100))
+#print(df_OT.head(100))
+
+
+print(df_equipe['MOTEUR'].unique())
+print(df_equipe['EQU_ID'].unique())
+
+print(df_OT['SIG_ORGANE'].unique())
+print(df_OT['ODR_LIBELLE'].value_counts())
+print(df_OT['TYPE_TRAVAIL'].unique())
+print(df_OT['EQU_ID'].unique())
+print(df_OT['SYSTEM_N1'].value_counts())
+
