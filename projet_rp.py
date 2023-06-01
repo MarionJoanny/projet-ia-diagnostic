9129ca2c4cafb53808bdf58dc1fe0b40b6a56ec4
rhn <heurlin.e2105233@etud.univ-ubs.fr>
2023-06-01 10:57:55 +0200

    description df

--- projet_rp.py
+++ projet_rp.py
@@ -8,11 +8,19 @@ df_equipe = pd.read_csv(file_equip, sep=";")
 df_OT = pd.read_csv(file_OT, sep=";")
 
 
+print('-------------equipe-------------------------')
+
+
 print(df_equipe.describe())
 print(df_equipe.head())
+print(f'isna = {df_equipe.isna().sum()}')
+
 print('--------------------------------------')
+
+print('----------------OT----------------------')
 
 print(df_OT.describe())
 print(df_OT.head())
+print(f'isna = {df_OT.isna().sum()}')
 print('--------------------------------------')
 
